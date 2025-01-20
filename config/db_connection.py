5cb30bc4a69227a9e7de9988a0887cfa4c77d3fd
Douglas Silva <douglasitpro@gmail.com>
2025-01-20 13:39:23 -0300

    Ajustando a logica para teste de conexão, chamando a função para teste

--- config/db_connection.py
+++ config/db_connection.py
@@ -15,7 +15,30 @@ def get_mongo_client():
         client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000) #timeout de 5 segundos
         #testando a conexão
         client.admin.command('ping')                                 #ping no servidor
-        print("COnexão com mongoDB estabelecida com sucesso.")
+        print("Conexão com mongoDB estabelecida com sucesso.")
         return client
     except errors.ServerSelectionTimeoutError as err:
         print(f'Erro ao conectar ao MongoDB: {err}')
+
+
+# para executar a função:
+if __name__ == "__main__":
+    try:
+        client = get_mongo_client()
+        print("Teste concluido: Conexão com MongoDB bem-sucedida!")
+    except Exception as e:
+        print(f"Erro no teste de conexão com MongoDB: {e}")
+
+
+"""
+Utilizar a conexão em outro pagina:
+
+from config.db_connection import get_mongo_client
+
+client = get_mongo_client()
+db = client['empresa_ficticia']
+clientes_collection = db['clientes']
+
+restante do codigo.........
+
+"""
